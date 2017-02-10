bc4c329551fefa6a6a2900dd79548cbb8ffa7541
jackzj <ybybzj@gmail.com>
2017-02-10 18:00:21 +0800

    first commit

---
+++ __test__/index.test.js
@@ -0,0 +1,5 @@
+var add = require('../index.js');
+
+test('test add function', () => {
+  expect(add(1, 2)).toBe(3);
+});
---
+++ index.js
@@ -0,0 +1,351 @@
+var makeBuilder = require('js-liber/fn/build');
+var t = require('js-liber/type');
+var pick = require('js-liber/l/pick');
+var http = require('http');
+var https = require('https');
+var url = require('url');
+var BPromise = require('bluebird');
+var assert = require('js-liber/assert');
+var extend = require('js-liber/extend');
+var qs = require('querystring');
+var pump = require('pump');
+var concatStream = require('concat-stream');
+var slice = require('js-liber/slice');
+var isStream = require('isstream');
+
+var HTTP_METHODS = [
+  'get',
+  'post',
+  'put',
+  'delete',
+  'patch',
+  'head'
+  //...
+];
+
+var reqx = makeBuilder(sendRequest, {
+  '?options': t.isObject,
+  '?uri': function(uri) {
+    return t.isString(uri) || t.isObject(uri);
+  },
+  '?sendType': [function(type) {
+    return ['html', 'xml', 'text', 'form', 'json'].indexOf(type) > -1;
+  }, 'form'],
+  '?method': [function(m) {
+    return t.isString(m) && HTTP_METHODS.indexOf(m.toLowerCase()) > -1;
+  }, 'get'],
+  '?headers': t.isObject,
+  '?agent': function(a) {
+    return a instanceof http.Agent || a instanceof https.Agent || a === false;
+  },
+  '?timeout': t.isNumber,
+  //options for ssl
+  '?pfx': isStrOrBuffer,
+  '?key': isStrOrBufferArr,
+  '?passphrase': t.isString,
+  '?cert': isStrOrBufferArr,
+  '?ca': isStrOrBufferArr,
+  '?ciphers': t.isString,
+  '?rejectUnauthorized': t.isBoolean,
+  '?secureProtocol': t.isString,
+  '?servername': t.isString
+}, {
+  fnName: 'reqx',
+  alias: {
+    build: 'send'
+  }
+});
+
+reqx = makeBuilder.mixin(reqx, HTTP_METHODS.reduce(function(mixins, method) {
+  var mname = method === 'delete' ? 'del' : method;
+
+  mixins[mname] = function $http_method(uri) {
+    return this.method(method).uri(uri);
+  };
+  return mixins;
+}, {}));
+
+reqx = makeBuilder.mixin(reqx, {
+  setHeader: function(key, value) {
+    var headers = this.headers() || {};
+    headers[key] = value;
+    return this.headers(headers);
+  }
+});
+
+module.exports = reqx;
+
+function sendRequest(options, data/*, builder*/) {
+  var _tmp = normalizeOptions(options, data);
+  var requestOptions = _tmp[0];
+  data = _tmp[1];
+  var requestFn = requestOptions.protocol === 'https:' ? https.request.bind(https) : http.request.bind(http);
+  var sendType = requestOptions.sendType;
+  var clientRequest, promise;
+  delete requestOptions.sendType;
+
+  console.dir(requestOptions, {
+    depth: null
+  });
+
+  clientRequest = requestFn(requestOptions);
+
+  promise = new BPromise(function(resolve, reject) {
+    clientRequest.on('response', function(response) {
+      if (response) {
+        // response.reqx = builder;
+        return resolve(response);
+      }
+      reject(new Error('[reqx]empty response!'));
+    });
+
+
+    if (!isStream.isReadable(data)) {
+      data = streamFrom(stringifyByType(sendType, data));
+    }
+
+    pump(data, clientRequest, function(err) {
+      if (err) reject(err);
+    });
+  });
+
+  clientRequest.response = promise;
+  // clientRequest.reqx = builder;
+
+  extendClinetRequest(clientRequest);
+
+  return clientRequest;
+}
+
+function extendClinetRequest(crq) {
+  extend(crq, {
+    onResponse: function(fn) {
+      return crq.response.then(fn);
+    },
+    _getPumpedQPromise: function() {
+      if (!crq._pumpPromise) {
+        crq._pumpPromise = this.onResponse(function(res) {
+          return [res, []];
+        });
+      }
+
+      return crq._pumpPromise;
+    },
+    pump: function( /*ws,..*/ ) {
+      var wss = slice(arguments, 0);
+      if (t.isArray(wss[0])) {
+        wss = wss[0];
+      }
+
+      crq._pumpPromise = crq._getPumpedQPromise().then(function(pair) {
+        var res = pair[0],
+          pumpedQ = pair[1];
+        var ss = wss.map(function(ws) {
+          return isStream(ws) ? ws : ws(res);
+        });
+
+        pumpedQ = pumpedQ.concat(ss);
+        return [res, pumpedQ];
+      });
+      return crq;
+    },
+    collect: function(collector) {
+      return crq._getPumpedQPromise().then(function(pair) {
+        var res = pair[0],
+          pumpedQ = pair[1];
+
+        return new BPromise(function(resolve, reject) {
+          var collectWs = t.isFunction(collector) ?
+            collector(res, resolve, reject) :
+            concatStream(function(body) {
+              resolve({
+                statusCode: res.statusCode,
+                response: res,
+                body: body
+              });
+            });
+
+          var pipeline = makePipeline(res, pumpedQ, collectWs);
+
+          pump(pipeline, function(err) {
+            if (err) return reject(err);
+          });
+        });
+      });
+    }
+  });
+  return crq;
+}
+
+//helpers
+function isStrOrBuffer(o) {
+  return t.isString(o) || Buffer.isBuffer(o);
+}
+
+function isStrOrBufferArr(o) {
+  return isStrOrBuffer(o) ||
+    (t.isArray(o) && (
+      o.every(isStrOrBuffer)
+    ));
+}
+
+function normalizeOptions(options, data) {
+  var requestOptions;
+
+  if (t.isObject(options.options)) {
+    requestOptions = options.options;
+  } else {
+    var uriObj = t.isString(options.uri) ? url.parse(options.uri) : options.uri;
+    assert(t.isObject(uriObj), '[reqx]invalid uri option! Given: ' + options.uri);
+
+    var uriOpts = pick(['protocol', 'host', 'hostname', 'port', 'path'], uriObj);
+
+    var restOptsKeys = uriOpts.protocol === ':https' ? ['sendType', 'method', 'headers', 'agent', 'timeout', 'pfx', 'key', 'passphrase', 'cert', 'ca', 'ciphers', 'rejectUnauthorized', 'secureProtocol', 'servername'] : ['sendType', 'method', 'headers', 'agent', 'timeout'];
+    var restOpts = pick(restOptsKeys, options);
+
+    if (needSendData(restOpts.method)) {
+      var headers = restOpts.headers || {};
+      headers['Content-Type'] = getReqCTypeStr[restOpts.sendType];
+      if (restOpts.sendType === 'json') {
+        headers['Accept'] = "application/json, text/*";
+      }
+      restOpts.headers = headers;
+    }
+    requestOptions = extend({}, uriOpts, restOpts);
+  }
+
+  if (!needSendData(requestOptions.method)) {
+    var pathObj = url.parse(requestOptions.path);
+    var searchStr = (pathObj.query ? (pathObj.query + '&') : '') + qs.stringify(data);
+    searchStr = searchStr ? ('?' + searchStr) : '';
+    pathObj.search = searchStr;
+    requestOptions.path = url.format(pathObj);
+    data = '';
+  }
+
+  return [requestOptions, data];
+
+}
+
+
+function getReqCTypeStr(type) {
+
+  return {
+    'html': 'text/html',
+    'xml': 'application/xml',
+    'text': 'text/*',
+    'form': 'application/x-www-form-urlencoded; charset=utf-8',
+    'json': 'application/json; charset=utf-8'
+  }[type];
+}
+
+function stringifyByType(type, o) {
+  if (t.isString(o)) {
+    return o;
+  }
+
+  if (o == null) {
+    return '';
+  }
+
+  if (type === 'json') {
+    return JSON.stringify(o);
+  }
+
+  return qs.stringify(o);
+}
+
+function needSendData(method) {
+  return method === 'post' || method === 'put' || method === 'patch';
+}
+
+var from = require('from2');
+
+function streamFrom(data) {
+  return from(function(size, next) {
+    if (data.length <= 0) {
+      return next(null, null);
+    }
+
+    var chunk = data.slice(0, size);
+    data = data.slice(size);
+    next(null, chunk);
+  });
+}
+
+function isWritableOnly(s) {
+  return isStream.isWritable(s) && !isStream.isReadable(s);
+}
+
+function makePipeline(res, pumpedQ, collectWs) {
+  var result = [res];
+  var i, l = pumpedQ.length,
+    pumped;
+  for (i = 0; i < l; i++) {
+    pumped = pumpedQ[i];
+
+    if (isWritableOnly(pumped)) {
+      collectWs = pumped;
+      break;
+    } else {
+      result.push(pumped);
+    }
+  }
+
+  result.push(collectWs);
+
+  return result;
+}
+
+// function isRedirect(statusCode) {
+//   return statusCode === 301 || statusCode === 302 || statusCode === 307 || statusCode === 308;
+// }
+
+// if (require.main === module) {
+
+
+// var through = require('through2');
+//   var request = reqx.get('https://www.google.com').send({
+//     a: 1,
+//     b: 2
+//   });
+
+//   var strThrough = through();
+//   strThrough.setEncoding('utf8');
+
+//   function redirect(request, maxCount) {
+//     maxCount = (isNaN(maxCount) || maxCount < 0) ? 10 : maxCount;
+
+//     function doRequest() {
+//       return request.onResponse(function(res) {
+//         if (maxCount > 0 && isRedirect(res.statusCode)) {
+//           maxCount -= 1;
+//           request = request.reqx.uri(res.headers.location).send();
+//           return doRequest();
+//         }
+//         return request;
+//       });
+//     }
+
+//     return doRequest();
+//   }
+
+
+//   redirect(request, 0).then(function(req) {
+//     req.pump(strThrough).collect(function(res, done) {
+//       return concatStream(function(body) {
+//         done({
+//           headers: res.headers,
+//           body: body
+//         });
+//       });
+//     }).then(console.log.bind(console)).catch(console.log.bind(console));
+//   });
+//   // .onResponse(function(res){
+//   // if(res.statusCode > 300 && res.statusCode < 400){
+//   //   return 
+//   // }
+//   // })
+
+//   // request.abort();
+// }
